edf9493dd745f267f3937c83827cae30e9a709d8
SarveshOO7 <sarveshtandon100@gmail.com>
2023-04-26 17:56:49 -0500

    Added some basic UI

--- lib/src/ui/device_interactor_screen.dart
+++ lib/src/ui/device_interactor_screen.dart
@@ -47,11 +47,15 @@ class DeviceInteractor extends StatefulWidget {
 
 class _DeviceInteractorState extends State<DeviceInteractor> {
   final Uuid _myServiceUuid =
-      Uuid.parse("19b10000-e8f2-537e-4f6c-6969768a1214");
+      Uuid.parse("1359814B-D0CC-4CBF-A41E-4BBD88676A11");
   final Uuid _myCharacteristicUuid =
-      Uuid.parse("19b10001-e8f2-537e-4f6c-6969768a1214");
+      Uuid.parse("5DBE15E7-DED0-47A8-86ED-ADB4B133BE74");
 
   Stream<List<int>>? subscriptionStream;
+  int score = 0;
+  final List<bool> _selectedOption = <bool>[true, false];
+  bool isSettingBoard = true;
+  bool isPlayingGame = false;
 
   @override
   Widget build(BuildContext context) {
@@ -96,11 +100,72 @@ class _DeviceInteractorState extends State<DeviceInteractor> {
                 builder: (context, snapshot) {
                   if (snapshot.hasData) {
                     print(snapshot.data);
-                    return Text(snapshot.data.toString());
+                    return Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(snapshot.data.toString()),
+                        GridView.count(
+                          shrinkWrap: true,
+                          crossAxisCount: 4,
+                          children: List.generate(
+                            16,
+                            (index) => Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: ElevatedButton(
+                                style: ButtonStyle(
+                                    backgroundColor:
+                                        MaterialStateProperty.all(Colors.blue)),
+                                child: Text("1"),
+                                // child: Container(
+                                //   height: 42.0,
+                                //   width: 42.0,
+                                //   color: Colors.red,
+                                // ),
+                                onPressed: () {
+                                  print("Button " +
+                                      index.toString() +
+                                      " pressed");
+                                },
+                              ),
+                            ),
+                          ),
+                        ),
+                        Text("Score:" + score.toString()),
+                        ToggleButtons(
+                          direction: Axis.horizontal,
+                          onPressed: (int index) {
+                            setState(() {
+                              // The button that is tapped is set to true, and the others to false.
+                              for (int i = 0; i < _selectedOption.length; i++) {
+                                _selectedOption[i] = i == index;
+                              }
+                              isSettingBoard = _selectedOption[0];
+                              isPlayingGame = _selectedOption[1];
+                            });
+                          },
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(8)),
+                          selectedBorderColor: Colors.blue[700],
+                          selectedColor: Colors.white,
+                          fillColor: Colors.blue[200],
+                          color: Colors.blue[400],
+                          constraints: const BoxConstraints(
+                            minHeight: 40.0,
+                            minWidth: 80.0,
+                          ),
+                          isSelected: _selectedOption,
+                          children: const [
+                            Text("Set Board"),
+                            Text("Play Game"),
+                          ],
+                        ),
+                      ],
+                    );
                   }
                   return const Text('No data yet');
                 })
-            : const Text('Stream not initalized')
+            : const Text('Stream not initalized'),
       ],
     );
   }
